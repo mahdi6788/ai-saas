b06ff473389819d46256b47751153c133855cd58
mahdi6788 <mjalalimech@gmail.com>
2025-05-12 18:25:19 +0330

    handle the image hosting problem

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "oaidalleapiprodscus.blob.core.windows.net"
+    ]
+  }
 };
 
 export default nextConfig;
