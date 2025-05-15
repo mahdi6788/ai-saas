8d94549b91a57bf286e7f9e62eb1db36fb28d4f3
mahdi6788 <mjalalimech@gmail.com>
2025-05-15 13:28:45 +0330

    refactor: enhance Navbar component to include subscription status in MobileSidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,20 @@ import { UserButton } from "@clerk/nextjs";
 
 import MobileSidebar from "@/components/MobileSidebar";
 
-export default function Navbar({apiLimitCount}:{apiLimitCount:number}) {
+export default function Navbar({
+  apiLimitCount,
+  isPro,
+}: {
+  apiLimitCount: number;
+  isPro: boolean;
+}) {
   return (
     <div className="flex items-center p-4">
       {/* Mobile sidebar toggle */}
-      <MobileSidebar apiLimitCount={apiLimitCount} />
+      <MobileSidebar apiLimitCount={apiLimitCount} isPro={isPro} />
       {/* Desktop Navber */}
       <div className="flex justify-end w-full">
-        <UserButton afterSwitchSessionUrl="/"/>
+        <UserButton afterSwitchSessionUrl="/" />
       </div>
     </div>
   );
